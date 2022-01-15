81c5220c86c9b756f76c2d7c2d08b552692a0e80
arjun-bhalekar <arjunbhalekar95@gmail.com>
2022-01-15 15:58:30 +0530

    changes till now

---
+++ spring/restful-web-services/src/main/java/com/learn/rest/webservices/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.learn.rest.webservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+
+	// @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World !";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean !");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		
+		return new HelloWorldBean("Hello World PathVariable : "+name);
+	}
+
+}
--- spring/restful-web-services/src/main/java/com/learn/rest/webservices/RestfulWebServicesApplication.java
+++ spring/restful-web-services/src/main/java/com/learn/rest/webservices/RestfulWebServicesApplication.java
@@ -8,6 +8,7 @@ public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
+		
 	}
 
 }
