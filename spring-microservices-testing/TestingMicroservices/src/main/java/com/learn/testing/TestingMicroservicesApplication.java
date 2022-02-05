a48f5282719c850f6eb4904b4f31b8038e412a08
arjun-bhalekar <arjunbhalekar95@gmail.com>
2022-02-05 14:34:50 +0530

    TestingMicroservice - Initial Project

---
+++ spring-microservices-testing/TestingMicroservices/src/main/java/com/learn/testing/TestingMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.learn.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingMicroservicesApplication.class, args);
+	}
+
+}
